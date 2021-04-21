acdeb63d0fbcb5be1e5e14c73ff36c88f58a850d
shaddock <shaddock@live.com>
2021-04-21 13:28:25 -0500

    repos

---
+++ TCC.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC.Domain.Entities;
+
+namespace TCC.Application.Contracts.Persistence
+{
+	public interface IOrderRepository : IAsyncRepository<Order>
+	{
+	}
+}
---
+++ TCC.Application/Contracts/Persistence/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC.Application.Contracts.Persistence;
+using TCC.Domain.Entities;
+
+namespace TCC.Application.Contracts.Persistence
+{
+	public interface IProductRepository : IAsyncRepository<Product>
+	{
+	}
+}
