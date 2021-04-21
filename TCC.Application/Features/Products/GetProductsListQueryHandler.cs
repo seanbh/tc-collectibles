767630d179e196dcd197ebf8c7f165dd3405b1c2
shaddock <shaddock@live.com>
2021-04-21 13:49:35 -0500

    product requests and handlers; mediatr; automapper

---
+++ TCC.Application/Features/Products/GetProductsListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TCC.Application.Contracts.Persistence;
+using TCC.Domain.Entities;
+
+namespace TCC.Application.Features.Products
+{
+	public class GetProductsListQueryHandler : IRequestHandler<GetProductsListQuery, List<ProductListVm>>
+	{
+		private readonly IMapper _mapper;
+		private readonly IAsyncRepository<Product> _productRepository;
+
+		public GetProductsListQueryHandler(IMapper mapper, IAsyncRepository<Product> productRepository)
+		{
+			_mapper = mapper;
+			_productRepository = productRepository;
+		}
+		public async Task<List<ProductListVm>> Handle(GetProductsListQuery request, CancellationToken cancellationToken)
+		{
+			var allProducts = (await _productRepository.ListAllAsync()).OrderBy(p => p.Name);
+			return _mapper.Map<List<ProductListVm>>(allProducts);
+		}
+	}
+}
---
+++ TCC.Application/Features/Products/ProductListVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCC.Application.Features.Products
+{
+	public class ProductListVm
+	{
+		public Guid ProductId { get; set; }
+		public string Name { get; set; }
+	}
+}
