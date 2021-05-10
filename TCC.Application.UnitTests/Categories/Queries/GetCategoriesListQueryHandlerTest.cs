97344814cc140df14a353a6cec0a20c9da23caff
shaddock <shaddock@live.com>
2021-05-10 13:29:40 -0500

    unit test

---
+++ TCC.Application.UnitTests/Categories/Queries/GetCategoriesListQueryHandlerTest.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TCC.Application.Contracts.Persistence;
+using TCC.Application.Features.Categories.Queries.GetCategoriesList;
+using TCC.Application.Profiles;
+using TCC.Application.UnitTests.Mocks;
+using Xunit;
+
+namespace TCC.Application.UnitTests.Categories.Queries
+{
+	public class GetCategoriesListQueryHandlerTest
+	{
+		private readonly Mock<ICategoryRepository> mockCategoryRepository;
+		private readonly IMapper mapper;
+
+		public GetCategoriesListQueryHandlerTest()
+		{
+			mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+			var configProvider = new MapperConfiguration(cfg =>
+			{
+				cfg.AddProfile<MappingProfile>();
+			});
+			mapper = configProvider.CreateMapper();
+		}
+
+		[Fact]
+		public async Task GetCategoriesListTest()
+		{
+			var handler = new GetCategoriesListQueryHandler(mapper, mockCategoryRepository.Object);
+
+			var categories= await handler.Handle(new GetCategoriesListQuery(), CancellationToken.None);
+
+			categories.ShouldBeOfType<List<CategoryListVm>>();
+			categories.Count.ShouldBe(4);
+		}
+	}
+}
---
+++ TCC.Application.UnitTests/Mocks/RepositoryMocks.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC.Application.Contracts.Persistence;
+using TCC.Domain.Entities;
+
+namespace TCC.Application.UnitTests.Mocks
+{
+	public class RepositoryMocks
+	{
+		public static Mock<ICategoryRepository> GetCategoryRepository()
+		{
+            var baseballCardGuid = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}");
+            var footballCardGuid = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}");
+            var basketballCardGuid = Guid.Parse("{BF3F3002-7E53-441E-8B76-F6280BE284AA}");
+            var hockeyCardGuid = Guid.Parse("{FE98F549-E790-4E9F-AA16-18C2292A2EE9}");
+
+			var mockCategories = new List<Category>
+			{
+				new Category
+				{
+					CategoryId = baseballCardGuid,
+					Name = "Baseball Cards"
+				},
+				new Category
+				{
+					CategoryId = footballCardGuid,
+					Name = "Football Cards"
+				},
+				new Category
+				{
+					CategoryId = basketballCardGuid,
+					Name = "Basketball Cards"
+				},
+				new Category
+				{
+					CategoryId = hockeyCardGuid,
+					Name = "Hockey Cards"
+				}
+			};
+
+			var mock = new Mock<ICategoryRepository>();
+            mock.Setup(m => m.ListAllAsync()).ReturnsAsync(mockCategories);
+
+			mock.Setup(m => m.AddAsync(It.IsAny<Category>())).ReturnsAsync((Category c) =>
+			{
+				mockCategories.Add(c);
+				return c;
+			});
+
+            return mock;
+        }
+	}
+}
