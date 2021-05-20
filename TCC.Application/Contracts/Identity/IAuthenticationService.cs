4a6e15909afc8ea525e8447932b67b841e232bb7
Sean Haddock <seanhaddock@live.com>
2021-05-20 09:46:14 -0500

    wip

---
+++ TCC.Application/Contracts/Identity/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC.Application.Models.Authentication;
+
+namespace TCC.Application.Contracts.Identity
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request);
+        Task<RegistrationResponse> RegisterAsync(RegistrationRequest request);
+    }
+}
---
+++ TCC.Identity/Services/AuthenticationService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC.Application.Contracts.Identity;
+using TCC.Application.Models.Authentication;
+using TCC.Identity.Models;
+
+namespace TCC.Identity.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IOptions<JwtSettings> jwtSettings;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AuthenticationService(UserManager<ApplicationUser> userManager, IOptions<JwtSettings> jwtSettings, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.jwtSettings = jwtSettings;
+            this.signInManager = signInManager;
+        }
+
+        public Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RegistrationResponse> RegisterAsync(RegistrationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
