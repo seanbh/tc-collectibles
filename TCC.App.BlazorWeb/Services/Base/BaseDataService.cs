c2a2b3b72684984099d5d7ffaae9c1b7fb7a9bd7
shaddock <shaddock@live.com>
2021-05-25 15:48:36 -0500

    fixed bearer token not working

--- TCC.App.BlazorWeb/Services/Base/BaseDataService.cs
+++ TCC.App.BlazorWeb/Services/Base/BaseDataService.cs
@@ -35,11 +35,13 @@ namespace TCC.App.BlazorWeb.Services.Base
         }
 
         protected async Task AddBearerToken()
-        {
-            var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzZWFuaGFkZG9jayIsImp0aSI6ImJjZDA5MzNmLTEwYmEtNDMzOC04MTc0LTU4MGJlY2JmNzcwZSIsImVtYWlsIjoic2VhbmhhZGRvY2tAbGl2ZS5jb20iLCJ1aWQiOiJmNTNlNDBkMi1jN2I3LTQyMTAtYmQ1Yi01ODI1NDFmMjE5ZDEiLCJleHAiOjE2MjE5NzczNzgsImlzcyI6IlRjY0lkZW50aXR5IiwiYXVkIjoiVGNjSWRlbnRpdHlVc2VyIn0.Ams1izDOXlH7OuUSjFaW834SpEB6gaSjdotg38bdJKo";
-            client.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            //if (await localStorage.ContainKeyAsync("token"))
-            //    client.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await localStorage.GetItemAsStringAsync("token"));
-        }
+        {                         
+            if (await localStorage.ContainKeyAsync("token"))
+            {
+                var token = await localStorage.GetItemAsStringAsync("token");
+                token = token.Replace("\"", ""); // not sure why this is necessary                
+                client.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+		}
     }
 }
