b823ced6467b327acf358964002360028f14aeeb
shaddock <shaddock@live.com>
2021-05-26 09:50:13 -0500

    wip

---
+++ TCC.Services.Catalog.Rest/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCC.Services.Catalog.Rest.Entities
+{
+	public class Product
+	{
+		public Guid ProductId { get; set; }
+		public string Name { get; set; }
+		public DateTime Year { get; set; }
+		public string ImageUrl { get; set; }
+		public Guid CategoryId { get; set; }
+		[Column(TypeName = "decimal(18,2)")]
+		public decimal Price { get; set; }
+	}
+}
