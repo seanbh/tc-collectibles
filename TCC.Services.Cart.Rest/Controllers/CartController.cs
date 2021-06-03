509058423d6be6fb6835d6b3216c19f6821bd692
Sean Haddock <seanhaddock@live.com>
2021-06-03 11:27:01 -0500

    wip

--- TCC.Services.Cart.Rest/Controllers/CartController.cs
+++ TCC.Services.Cart.Rest/Controllers/CartController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TCC.Services.Cart.Rest.Models;
+using TCC.Services.Cart.Rest.Respositories;
 
 namespace TCC.Services.Cart.Rest.Controllers
 {
@@ -11,5 +14,39 @@ namespace TCC.Services.Cart.Rest.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+        private readonly ICartRepository cartRepository;
+        private readonly IMapper mapper;
+
+        public CartController(ICartRepository cartRepository, IMapper mapper)
+        {
+            this.cartRepository = cartRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet("{cartId}", Name ="GetCart")]
+        public async Task<ActionResult<Models.Cart>> Get(Guid cartId)
+        {
+            var cart = await cartRepository.GetCartById(cartId);
+            if(cart == null)
+            {
+                return NotFound();
+            }
+
+            Models.Cart cartModel = mapper.Map<Models.Cart>(cart);
+            cartModel.NumberOfItems = cart.CartLines.Sum(cl => cl.Qty);
+            return base.Ok(cartModel);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Models.Cart>> Post(CartForCreation cartModel)
+        {
+            var cart = mapper.Map<Entities.Cart>(cartModel);
+            cartRepository.AddCart(cart);
+            await cartRepository.SaveChanges();
+
+            var returnModel = mapper.Map<Models.Cart>(cart);
+
+            return CreatedAtRoute("GetCart", new { cartId = cart.CartId }, returnModel);
+        }
     }
 }
--- TCC.Services.Cart.Rest/Controllers/CartLinesController.cs
+++ TCC.Services.Cart.Rest/Controllers/CartLinesController.cs
@@ -1,15 +1,64 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TCC.Services.Cart.Rest.Respositories;
+using TCC.Services.Cart.Rest.Services;
 
 namespace TCC.Services.Cart.Rest.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/carts/{cartId}/cartLines")]
     [ApiController]
     public class CartLinesController : ControllerBase
     {
+        private readonly ICartLinesRepository cartLinesRepository;
+        private readonly ICartRepository cartRepository;
+        private readonly IProductRepository productRepository;
+        private readonly ICatalogService catalogService;
+        private readonly IMapper mapper;
+
+        public CartLinesController(ICartLinesRepository cartLinesRepository, ICartRepository cartRepository, IProductRepository productRepository, ICatalogService catalogService, IMapper mapper)
+        {
+            this.cartLinesRepository = cartLinesRepository;
+            this.cartRepository = cartRepository;
+            this.productRepository = productRepository;
+            this.catalogService = catalogService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.CartLine>>> Get(Guid cartId)
+        {
+            if(!await cartRepository.CartExists(cartId))
+            {
+                return NotFound();
+            }
+
+            var cartLines = await cartLinesRepository.GetCartLines(cartId);
+            var cartLineModels = mapper.Map<IEnumerable<Models.CartLine>>(cartLines);
+
+            return Ok(cartLineModels);
+        }
+
+        [HttpGet("{cartLineId}", Name = "GetCartLine")]
+        public async Task<ActionResult<Models.CartLine>> Get(Guid cartId, Guid cartLineId)
+        {
+            if(!await cartRepository.CartExists(cartId))
+            {
+                return NotFound();
+            }
+
+            var cartLine = await cartLinesRepository.GetCartLineById(cartLineId);
+            if(cartLine == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mapper.Map<Models.CartLine>(cartLine));
+        }
+
     }
 }
